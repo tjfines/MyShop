2d3e9e29aaac4b6784a39b641c6b4fd04ccd38bd
Tjfines <46894265+tjfines@users.noreply.github.com>
2020-09-21 08:15:47 -0400

    Implemented generic Repository

--- MyShop/MyShop.Core/Models/BaseEntity.cs
+++ MyShop/MyShop.Core/Models/BaseEntity.cs
@@ -6,14 +6,15 @@ using System.Threading.Tasks;
 
 namespace MyShop.Core.Models
 {
-    public class ProductCategory
+    public abstract class BaseEntity
     {
         public string Id { get; set; }
-        public string Category { get; set; }
+        public DateTimeOffset CreatedAt { get; set; }
 
-        public ProductCategory()
+        public BaseEntity()
         {
             this.Id = Guid.NewGuid().ToString();
+            this.CreatedAt = DateTime.Now;
         }
     }
 }
